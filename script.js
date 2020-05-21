b92dbe568fba5cabbbb3ab8f574e16e023b1b8ef
Brendon Henning <brendonhenning@gmail.com>
2020-05-21 17:00:02 -0600

    Added setTime function so clock can be started any time after page is loaded.

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
-const startWork = document.querySelector('#work-time-start')
-startWork.addEventListener('click', startWorkTime)
-let now = new Date().getTime();
-let totalTime = 1500000 // minutes * 60 * 1000.
-let deadline = now + totalTime;
+const startWorkButton = document.querySelector('#work-time-start')
+let deadline = undefined;
+function runWorkInterval() {
+        const workTime = document.querySelector('#work-time-display')
+        let current = new Date().getTime();
+        let timeLeft = deadline - current;
+        let seconds = Math.floor((timeLeft / 1000) % 60);
+        let minutes = Math.floor(timeLeft / (1000 * 60));
+        workTime.textContent = `${minutes}` + ':' + `${seconds}`
+        if (timeLeft < 0) {
+            clearInterval();
+        }
+}
+function noDelay(func, interval) {
+    func();
+    return setInterval(func, interval)
+}
 function startWorkTime() {
-        let countdown = setInterval(function() {
-       
+    noDelay(runWorkInterval, 1000)
 }
-function clockStart() {
-    let current = new Date().getTime();
-    let timeLeft = deadline - current;
-    let seconds = Math.floor((timeLeft / 1000) % 60);
-    let minutes = Math.floor(timeLeft / (1000 * 60));
-    const workTime = document.querySelector('#work-time-display')
-    workTime.textContent = `${minutes}` + ':' + `${seconds}`
-    if (timeLeft < 0) {
-        clearInterval(countdown);
-    }
-}, 1000);
+startWorkButton.addEventListener('click', () => {
+    setTime();
+    startWorkTime();
+});
+function setTime() {
+    let now = new Date().getTime();
+    let totalTime = 1500000 // minutes * 60 * 1000.
+    deadline = now + totalTime;
+    return deadline;
 }
-
