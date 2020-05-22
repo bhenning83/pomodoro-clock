94e6209ad137ea2d5c032148aff10c33b103fa89
Brendon Henning <brendonhenning@gmail.com>
2020-05-21 20:32:42 -0600

    work and play clock both work. No delay.

--- script.js
+++ script.js
@@ -1,30 +1,60 @@
+//WORK CLOCK
 const startWorkButton = document.querySelector('#work-time-start')
-let deadline = undefined;
+let workDeadline = undefined;
 function runWorkInterval() {
-        const workTime = document.querySelector('#work-time-display')
-        let current = new Date().getTime();
-        let timeLeft = deadline - current;
-        let seconds = Math.floor((timeLeft / 1000) % 60);
-        let minutes = Math.floor(timeLeft / (1000 * 60));
-        workTime.textContent = `${minutes}` + ':' + `${seconds}`
-        if (timeLeft < 0) {
-            clearInterval();
-        }
-}
-function noDelay(func, interval) {
-    func();
-    return setInterval(func, interval)
-}
-function startWorkTime() {
-    noDelay(runWorkInterval, 1000)
+        const workTimeDisplay = document.querySelector('#work-time-display')
+        let workCountdown = setInterval(function(){
+            let current = new Date().getTime();
+            let timeLeft = workDeadline - current;
+            let minutes = Math.floor(timeLeft / (1000 * 60));
+            if (minutes < 10) minutes = '0' + minutes;
+            let seconds = Math.floor((timeLeft / 1000) % 60);
+            if (seconds < 10) seconds = '0' + seconds;
+            workTimeDisplay.textContent = `${minutes}` + ':' + `${seconds}`;
+            if (timeLeft <= 0) {
+                clearInterval(workCountdown);
+                workTimeDisplay.textContent = '00:00'
+            }
+        });
+        
 }
 startWorkButton.addEventListener('click', () => {
-    setTime();
-    startWorkTime();
+    setWorkTime();
+    runWorkInterval();
 });
-function setTime() {
+function setWorkTime() {
     let now = new Date().getTime();
-    let totalTime = 1500000 // minutes * 60 * 1000.
-    deadline = now + totalTime;
-    return deadline;
+    let totalTime = (5000)// minutes * 60 * 1000.
+    workDeadline = now + totalTime;
+    return workDeadline;
+}
+//PLAY CLOCK
+const startPlayButton = document.querySelector('#play-time-start')
+let playDeadline = undefined;
+function runPlayInterval() {
+        const playTimeDisplay = document.querySelector('#play-time-display')
+        let playCountdown = setInterval(function(){
+            let current = new Date().getTime();
+            let timeLeft = playDeadline - current;
+            let minutes = Math.floor(timeLeft / (1000 * 60));
+            if (minutes < 10) minutes = '0' + minutes;
+            let seconds = Math.floor((timeLeft / 1000) % 60);
+            if (seconds < 10) seconds = '0' + seconds;
+            playTimeDisplay.textContent = `${minutes}` + ':' + `${seconds}`;
+            if (timeLeft <= 0) {
+                clearInterval(playCountdown);
+                playTimeDisplay.textContent = '00:00'
+            }
+        });
+        
+}
+startPlayButton.addEventListener('click', () => {
+    setPlayTime();
+    runPlayInterval();
+});
+function setPlayTime() {
+    let now = new Date().getTime();
+    let totalTime = (5000)// minutes * 60 * 1000.
+    playDeadline = now + totalTime;
+    return playDeadline;
 }
